5e4e35a3aa357e27e1b15cd2188ae908efcc4339
Ro.Oliveira <conquistajoaovictor@gmail.com>
2023-09-11 18:04:12 -0300

    feat/database connection w sequelize

--- api-produtos/src/db/strategies/postgres.js
+++ api-produtos/src/db/strategies/postgres.js
@@ -1,13 +1,81 @@
-const ICrud = require('./interface/interfaceCrud');
+const ICrud = require("./interface/interfaceCrud");
+const Sequelize = require("sequelize");
 
 class PostGres extends ICrud {
-    constructor() {
-        super();
+  constructor() {
+    super();
+    this._driver = null;
+    this._produtos = null;
+    this._connect();
+  }
+  async isConnected(){
+    try {
+        this._driver.authenticate()
+        return true;
+    } catch (error) {
+        console.log('Erroouu: ', error)
     }
+  }
 
-    create(item) {
-        console.log('O produto foi salvo em PostGres');    
-    }  
+  async _defineModel() {
+    this._produtos = drive.define("produtos", {
+        id_produto: {
+          type: Sequelize.INTEGER,
+          required: true,
+          primaryKey: true,
+          autoIncrement: true,
+        },
+        nome_produto: {
+          type: Sequelize.STRING,
+          required: true,
+        },
+        descricao: {
+          type: Sequelize.STRING,
+          required: true,
+        },
+        preco: {
+          type: Sequelize.DOUBLE,
+          required: true,
+        },
+        quantidade: {
+          type: Sequelize.INTEGER,
+          required: true,
+        },
+        categoria: {
+            type: Sequelize.STRING,
+            required: true,
+        }
+      },{
+        tableName: 'TB_PRODUTOS',
+        freezeTableName: false,//não alterar o nome da tabela
+        timestamps: false//não criar colunas de controle de data
+      });
+    
+      await Produtos.sync();
+  }
+
+  _connect() {
+    this._driver = new Sequelize(
+      "produtos", //database
+      "JoaoConquistaDev", //user
+      "123456", //password
+      {
+        host: "localhost", // onde está hospedado
+        dialect: "postgres", //drive type
+        //case sentive
+        quoteIdentifiers: false,
+        //deprecation warining
+        operatorsAliases: false,
+        // dialectOptions: {
+        //     ssl: false,
+        // }
+      }
+    );
+  }
+
+  create(item) {
+    console.log("O produto foi salvo em PostGres");
+  }
 }
 
 module.exports = PostGres;
