bd598a81306cbde58efad95db7ef16151159eb90
Ro.Oliveira <conquistajoaovictor@gmail.com>
2023-09-12 15:56:35 -0300

    feat/delete and update

--- api-produtos/src/tests/postgresStrategy.test.js
+++ api-produtos/src/tests/postgresStrategy.test.js
@@ -9,6 +9,14 @@ const MOCK_PRODUTO_CADASTRAR = {
     quantidade: 10.00,
     categoria: 'Categoria Teste'
 }
+const MOCK_PRODUTO_ATUALIZAR = {
+    id_produto: 4,
+    nome_produto: 'Produto Teste Atualizado',
+    descricao: 'Produto Teste Atualizado',
+    preco: 20.00,
+    quantidade: 20.00,
+    categoria: 'Categoria Teste Atualizado'
+}
 
 const context = new ContextStrategy(new PostGres());
 
@@ -33,5 +41,21 @@ describe('Postgres Strategy', function () {
         const [result] = await context.read(MOCK_PRODUTO_CADASTRAR)
         delete result.id_produto;
         assert.deepEqual(result, MOCK_PRODUTO_CADASTRAR)
+    })
+
+    it('Update Product', async function () {
+        const [update] = await context.read({nome_produto: MOCK_PRODUTO_ATUALIZAR.nome_produto});
+        const novoItem = {
+            ...MOCK_PRODUTO_ATUALIZAR,
+            nome_produto: 'Produto Teste Atualizado'
+        }
+        const [result] = await context.update(update.id_produto, novoItem);
+        assert.deepEqual(result, 1);
+    })
+
+    it('Delete Product', async function () {
+        const [item] = await context.read({});
+        const result = await context.delete(item.id_produto);
+        assert.deepEqual(result, 1)
     })
 })
