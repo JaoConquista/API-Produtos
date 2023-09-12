08e965d54a2d89d0bd1c191b685fbe88ae2ec2b2
Ro.Oliveira <conquistajoaovictor@gmail.com>
2023-09-12 10:41:37 -0300

    feat: DB connection test

--- api-produtos/src/tests/postgresStrategy.test.js
+++ api-produtos/src/tests/postgresStrategy.test.js
@@ -3,10 +3,10 @@ const PostGres = require('../db/strategies/postgres')
 const ContextStrategy = require('../db/strategies/base/contextStrategy')
 
 const context = new ContextStrategy(new PostGres());
-
 describe('Postgres Strategy', function () {
     this.timeout(Infinity);
     it('Postgres SQL Connection', async function (){
-        
+        const result = await context.isConnected();
+        assert.equal(result, true)
     })
 })
