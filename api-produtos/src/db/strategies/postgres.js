1f9d17b4b3a5b11bb3e118227b3b179d0d5c84cf
Raul Oliveira <mrraul65@gmail.com>
2023-09-15 15:58:41 -0300

    identation

--- api-produtos/src/db/strategies/postgres.js
+++ api-produtos/src/db/strategies/postgres.js
@@ -7,18 +7,20 @@ class PostGres extends ICrud {
     this._driver = null;
     this._produtos = null;
   }
-  
-  async isConnected(){
+
+  async isConnected() {
     try {
-        this._driver.authenticate()
-        return true;
+      this._driver.authenticate();
+      return true;
     } catch (error) {
-        console.log('Erroouu: ', error)
+      console.log("Erroouu: ", error);
     }
   }
 
   async _defineModel() {
-    this._produtos = this._driver.define("produtos", {
+    this._produtos = this._driver.define(
+      "produtos",
+      {
         id_produto: {
           type: Sequelize.INTEGER,
           required: true,
@@ -42,54 +44,61 @@ class PostGres extends ICrud {
           required: true,
         },
         categoria: {
-            type: Sequelize.STRING,
-            required: true,
-        }
-      },{
-        tableName: 'TB_PRODUTOS',
-        freezeTableName: false,//não alterar o nome da tabela
-        timestamps: false//não criar colunas de controle de data
-      });
-    
-      await this._produtos.sync();
+          type: Sequelize.STRING,
+          required: true,
+        },
+      },
+      {
+        tableName: "TB_PRODUTOS",
+        freezeTableName: false, //não alterar o nome da tabela
+        timestamps: false, //não criar colunas de controle de data
+      }
+    );
+
+    await this._produtos.sync();
   }
 
   async connect() {
     this._driver = new Sequelize(
-      "produtos", //database
-      "JoaoConquistaDev", //user
-      "123456", //password
+      process.env.PRODUCTS_TABLE || "produtos", //table
+      process.env.POSTGRES_USER || "dev", //user
+      process.env.POSTGRES_PASSWORD || "dev", //password
       {
         host: "localhost", // onde está hospedado
         dialect: "postgres", //drive type
         //case sentive
         quoteIdentifiers: false,
-        logging: false
+        logging: false,
       }
-    )
+    );
     await this._defineModel();
   }
 
-  async  create(item) {
-   const {dataValues} = await this._produtos.create(item);
+  async create(item) {
+    const { dataValues } = await this._produtos.create(item);
 
-   return dataValues;
+    return dataValues;
   }
 
-  async read (item){
-    return this._produtos.findAll({where: item, raw: true});
+  async read(item) {
+    return this._produtos.findAll({ where: item, raw: true });
   }
 
-  async update(id, item){
-    const r = await this._produtos.update(item, {where: {id_produto : id}})
+  async update(id, item) {
+    const r = await this._produtos.update(item, { where: { id_produto: id } });
     return r;
   }
 
-  async delete(id){
-    const query = id ? {id_produto: id} : {};
-    return this._produtos.destroy({where: query});
+  async delete(id) {
+    const query = id ? { id_produto: id } : {};
+    return this._produtos.destroy({ where: query });
   }
-  
+
+  async bulkCreate(items) {
+    const values = await this._produtos.bulkCreate(items, { returning: true });
+
+    return values;
+  }
 }
 
 module.exports = PostGres;
